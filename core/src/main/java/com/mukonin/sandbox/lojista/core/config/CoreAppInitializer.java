bfd5aa2f1b59522e352a3fd19d96a74a53ba348d
mukonin <alex.mukonin@gmail.com>
2019-07-16 17:20:45 +0300

    T-01 maven project
    maven project skeleton
    spring project skeleton
    gitignore file

---
+++ core/src/main/java/com/mukonin/sandbox/lojista/core/config/CoreAppInitializer.java
@@ -0,0 +1,17 @@
+package com.mukonin.sandbox.lojista.core.config;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletException;
+
+/**
+ * Place core application configuration here, then remove this description
+ */
+public class CoreAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+
+    }
+
+}

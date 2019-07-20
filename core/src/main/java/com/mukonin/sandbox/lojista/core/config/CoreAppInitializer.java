9eabf520c96043db03995654337f26f31bcb97b7
Roman Movcheniuk <movcheniuk.roma@gmail.com>
2019-07-20 16:34:21 +0300

    T-02 Added Spring and Thymeleaf config

--- core/src/main/java/com/mukonin/sandbox/lojista/core/config/CoreAppInitializer.java
+++ core/src/main/java/com/mukonin/sandbox/lojista/core/config/CoreAppInitializer.java
@@ -1,17 +1,25 @@
 package com.mukonin.sandbox.lojista.core.config;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 /**
- * Place core application configuration here, then remove this description
+ * Core application configuration
  */
 public class CoreAppInitializer implements WebApplicationInitializer {
 
     @Override
-    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
-
+    public void onStartup(javax.servlet.ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(ThymeleafConfig.class);
+        context.setServletContext(servletContext);
+        // Spring MVC front controller
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
     }
 
 }
---
+++ core/src/main/java/com/mukonin/sandbox/lojista/core/config/ThymeleafConfig.java
@@ -0,0 +1,59 @@
+package com.mukonin.sandbox.lojista.core.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+/**
+ * Thymeleaf and Spring MVC configuration.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.mukonin")
+public class ThymeleafConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public ViewResolver viewResolver(
+            TemplateEngine templateEngine) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
+
+    @Bean
+    public TemplateEngine templateEngine(
+            ITemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver =
+                new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+}

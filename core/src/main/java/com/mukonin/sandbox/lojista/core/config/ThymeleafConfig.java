aa681fbf68e40387bd1f59a74ea5eeaaa68bca56
Roman Osadchuk <osadchuk.roma.26@gmail.com>
2019-08-28 21:15:51 +0300

    T-06 (S-04) Operator UI

--- core/src/main/java/com/mukonin/sandbox/lojista/core/config/ThymeleafConfig.java
+++ core/src/main/java/com/mukonin/sandbox/lojista/core/config/ThymeleafConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -29,6 +30,11 @@ public class ThymeleafConfig extends WebMvcConfigurerAdapter implements Applicat
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");
     }
 
     @Bean
--- core/src/main/java/com/mukonin/sandbox/lojista/core/controller/OperatorController.java
+++ core/src/main/java/com/mukonin/sandbox/lojista/core/controller/OperatorController.java
@@ -1,14 +1,12 @@
 package com.mukonin.sandbox.lojista.core.controller;
 
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.mukonin.sandbox.lojista.core.service.ParcelService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 /**
  * Intended to verify security functionality
@@ -17,13 +15,16 @@ import java.time.LocalTime;
 @Controller
 @RequestMapping("/opr")
 public class OperatorController {
+    private final ParcelService parcelService;
+
+    public OperatorController(ParcelService parcelService) {
+        this.parcelService = parcelService;
+    }
 
     @GetMapping
-    public String index(Model model){
-        model.addAttribute("date", LocalDate.now());
-        model.addAttribute("time", LocalTime.now());
-        SecurityContext context = SecurityContextHolder.getContext();
-        model.addAttribute("user", context.getAuthentication().getName());
+    public String index(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        model.addAttribute("parcels", parcelService.getAllAsParcelDTO());
+        model.addAttribute("user", userDetails.getUsername());
         return "operator";
     }
 }
